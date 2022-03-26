477b2cd1251173c4208fbf2ac70c36cac8acb767
Sriram <sriramanvellingiri@gmail.com>
2022-03-26 14:56:49 +0530

    fix(i18n): add translations for the components (#17)
    * fix(i18n): add translations for the components

--- react/src/components/blocks/Modals/CreateChannel/CreateChannel.js
+++ react/src/components/blocks/Modals/CreateChannel/CreateChannel.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import styles from './CreateChannel.styles.css'
 import { Modal, InputTextField } from '@/ui'
+import { useI18n } from '@/i18n'
 
 function CreateChannel({ show, handleModalClose }) {
+  const { formatMessage } = useI18n()
   return (
     <>
-      <Modal show={show} handleModalClose={handleModalClose} title={'Create a channel'}>
-        <p className={styles.heading}>Channels are where your team communicates. They’re best when organized around a topic — #marketing, for example.</p>
+      <Modal show={show} handleModalClose={handleModalClose} title={formatMessage({ id: 'create_a_channel' })}>
+        <p className={styles.heading}>{formatMessage({ id: 'channel_description' })}</p>
         <div>
-          <label className={styles.label}>Name</label>
-          <InputTextField className={styles.textField} placeholder="e.g. plan-budget" />
+          <label className={styles.label}>{formatMessage({ id: 'name' })}</label>
+          <InputTextField className={styles.textField} placeholder={formatMessage({ id: 'plan_budget' })} />
         </div>
       </Modal>
     </>
