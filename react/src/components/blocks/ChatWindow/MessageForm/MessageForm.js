e1f70bd4f7797a33795d69856facc18b4cc2b87f
sriramanvellingiri <sriramanvellingiri@gmail.com>
2022-03-28 23:22:46 +0530

    feat(chatwindow/editor): add  editor feature

--- react/src/components/blocks/ChatWindow/MessageForm/MessageForm.js
+++ react/src/components/blocks/ChatWindow/MessageForm/MessageForm.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './MessageForm.styles.css'
 
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+
+
+
 function MessageForm() {
+  const [editorState, setEditorState] = useState(
+    () => EditorState.createEmpty(),
+  );
   return (
     <>
-      <p className={styles.line}>MessageForm goes here.</p>
+      <div className={styles.feedForm}>
+      <Editor
+          editorState={editorState}
+          onChange={setEditorState}
+          wrapperClassName={styles.WrapperClass}
+          editorClassName={styles.EditorClass}
+          toolbarClassName={styles.ToolbarClass}
+        />
+      </div>
+     
     </>
   )
 }
