1d899070811f30099151c2819d2fabcc54ab1277
Tushar Gupta <cse.tushar@gmail.com>
2022-03-30 07:05:33 -0500

    tech(hook/services): add react custom hook services

---
+++ react/src/utils/endpoints.js
@@ -0,0 +1,18 @@
+function getBasePath() {
+  return 'https://slackclone001.herokuapp.com'
+}
+
+const ORGANIZATION_ID = '1ae5241e-a51b-11ec-b909-0242ac120002'
+const CHANNEL_ID = '4f0300c2-a51b-11ec-b909-0242ac120002'
+
+export const USERS_LIST = `${getBasePath()}/users`
+
+export const CHANNEL_LIST = `${getBasePath()}/channel`
+
+export const MESSAGES_LIST = `${getBasePath()}/messages/${CHANNEL_ID}/${ORGANIZATION_ID}`
+
+export const ORGANIZATION_BY_ID = `${getBasePath()}/organization/${ORGANIZATION_ID}`
+
+export const CREATE_CHANNEL = `${getBasePath()}/channel`
+
+export const ADD_USERS = `${getBasePath()}/addusers`
