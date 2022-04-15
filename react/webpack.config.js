89b8a9c0f49456179dc3d456d4c4910672f7f500
sriramanvellingiri <sriramanvellingiri@gmail.com>
2022-04-15 06:39:28 +0530

    tech(component/sidebar): add contextual menu

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -25,7 +25,6 @@ module.exports = {
             },
           },
         ],
-        exclude: [path.resolve(__dirname, 'src/css/react-draft-wysiwyg.css')],
       },
       {
         test: /.(js)$/,
