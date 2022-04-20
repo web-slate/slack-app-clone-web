2f22e93d887f4f9b4dc93b3b393a9e3c174681c7
sriramanvellingiri <sriramanvellingiri@gmail.com>
2022-04-20 07:27:29 +0530

    fix(component/chatwindow): Static image for the header

--- react/src/components/blocks/ChatWindow/ChatHeader/ChannelMembers/ChannelMembers.js
+++ react/src/components/blocks/ChatWindow/ChatHeader/ChannelMembers/ChannelMembers.js
@@ -6,20 +6,8 @@ import userIconImg from '../../../../../static/images/usericon.png'
 function ChannelMembers({ members = [] }) {
   return (
     <div className={styles.channelMembers}>
-      {members &&
-        members.map((member, index) => {
-          return (
-            <img
-              key={index}
-              src={index % 2 ? userImg : userIconImg}
-              alt={member.username}
-              title={member.username}
-              width="100%"
-              height="100%"
-              className={styles.memberImg}
-            />
-          )
-        })}
+      <img src={userImg} alt="user1" width="100%" height="100%" className={styles.memberImg}/>
+      <img src={userIconImg} alt="user1" width="100%" height="100%" className={styles.memberImg}/>
       <span className={styles.memberCount}> {members.length} </span>
     </div>
   )
